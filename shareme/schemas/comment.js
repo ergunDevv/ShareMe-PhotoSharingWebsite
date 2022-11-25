8dc3b3dd8bc9ae9be378a2a21a22dd4a513ce334
ergunDevv <ergncyln2127088@gmail.com>
2022-11-25 19:32:32 +0300

    Sanity backend

---
+++ shareme/schemas/comment.js
@@ -0,0 +1,18 @@
+export default {
+    name:"comment",
+    title:"Commet",
+    type:"document",
+    fields:[
+        {
+        name:"postedBy",
+        title:"PostedBy",
+        type:"postedBy"
+        },
+        {
+        name:"comment",
+        title:"Comment",
+        type:"string"
+        },
+        
+    ]
+}

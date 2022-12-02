c4d8bbb86adf87bbc40de3c5d2f2dd00324f0f42
ergunDevv <ergncyln2127088@gmail.com>
2022-12-02 12:59:44 +0300

    Error solved. Feed routes created.

--- shareme_frontend/src/components/Feed.js
+++ shareme_frontend/src/components/Feed.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-
-const Navbar = ()=>{
+const Feed = ()=>{
     return(
         <div>
-            Navbar
+            Feed
         </div>
     )
 }
 
-export default Navbar
+export default Feed

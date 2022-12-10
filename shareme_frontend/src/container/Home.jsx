5039df67af1574ed7c2cc51907bcb718d15376fa
ergunDevv <ergncyln2127088@gmail.com>
2022-12-10 12:30:44 +0300

    UserProfile Final Version

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -64,7 +64,7 @@ function Home() {
 
       <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
         <Routes>
-          <Route path="/user-profile:userId" element={<UserProfile />} />
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
           <Route path="/*" element={<Pins user={user && user} />} />
         </Routes>
       </div>
